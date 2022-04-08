14e14f9e0db4cc5f47f1f890ef5ad5731715d74e
kemellynasc <72822516+kemellynasc@users.noreply.github.com>
2022-04-08 17:44:17 -0300

    Atividade de Polimorfismo em Java 08/04/2022

---
+++ polimorfismo/Cachorro.java
@@ -0,0 +1,33 @@
+package polimorfismo;
+
+public class Cachorro extends Animal {
+
+	public Cachorro() {
+		super("Tipo Animal: Cachorro");
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void nome(String nomeAnimal)
+	{
+		System.out.println("\nNome do cachorro: "+nomeAnimal);
+	}
+	
+	@Override
+	public void idade(int idadeAnimal)
+	{
+		System.out.println("\nIdade: "+idadeAnimal);
+	}
+	
+	@Override
+	public void som(String somAnimal)
+	{
+		System.out.println("\nSom que seu cachorro emite: "+somAnimal);
+	}
+	
+	public void Corre()
+	{
+		System.out.println("\nUma das características do cachorro é correr!!!");
+	}
+
+	
+}
